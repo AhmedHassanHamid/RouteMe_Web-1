a981316fffd94f1677cd8b6cb7813febfcaabe5e
marian6721 <82811290+marian6721@users.noreply.github.com>
2022-03-08 00:10:01 +0200

    dispatcher page

---
+++ lib/Model/Drivers/driver.dart
@@ -0,0 +1,9 @@
+class DriversModel {
+  final String status;
+  final String email;
+  final String name;
+  final String phone;
+  final Map<String, dynamic> actions;
+
+  DriversModel(this.status, this.email, this.name, this.phone, this.actions);
+}
